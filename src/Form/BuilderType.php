c58f9b55880bf09cc8295b1bdb5a99e364fcad16
Tobopro <tompro.boyer@gmail.com>
2024-02-15 10:42:05 +0100

    building the form

--- src/Form/BuilderType.php
+++ src/Form/BuilderType.php
@@ -7,13 +7,40 @@ use App\Entity\Levels;
 use App\Entity\SubClasses;
 use App\Entity\SubRaces;
 use App\Entity\User;
+use App\Repository\SubClassesRepository;
+use App\Repository\SubRacesRepository;
+use App\Repository\ClassesRepository;
+use App\Repository\RacesRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class BuilderType extends AbstractType
 {
+
+    private $subClassesRepository;
+    private $subRacesRepository;
+    private $classesRepository;
+    private $racesRepository;
+
+
+    public function __construct(SubClassesRepository $subClassesRepository,
+     SubRacesRepository $subRacesRepository,
+     ClassesRepository $classesRepository,
+     RacesRepository $racesRepository) 
+     
+    {
+        $this->subClassesRepository = $subClassesRepository;
+        $this->subRacesRepository = $subRacesRepository;
+        $this->classesRepository = $classesRepository;
+        $this->racesRepository = $racesRepository;
+
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -26,21 +53,40 @@ class BuilderType extends AbstractType
             ->add('charisma')
             ->add('abilityScoreBonus1')
             ->add('abilityScoreBonus2')
-            ->add('idSubRace', EntityType::class, [
-                'class' => SubRaces::class,
-'choice_label' => 'name',
+            ->add('idSubRace', ChoiceType::class, [
+                'choices' => $this->subRacesRepository->findAll(),
+                'choice_label' => 'name',
+                'expanded' => true,
+                'label'=> ' ',
+                // 'attr' => ['class' => 'checkmark']
             ])
-            ->add('idSubClasses', EntityType::class, [
-                'class' => SubClasses::class,
-'choice_label' => 'name',
+            ->add('idSubClasses', ChoiceType::class, [
+                'choices' => $this->subClassesRepository->findAll(),
+                'choice_label' => 'name',
+                'expanded' => true,
+                'label'=> ' ',
+            ])
+             ->add('idClasses', ChoiceType::class, [
+                'choices' => $this->classesRepository->findAll(),
+                'choice_label' => 'name',
+                'expanded' => true,
+                'label'=> ' ',
+                'mapped'=>false
+            ])
+             ->add('idRaces', ChoiceType::class, [
+                'choices' => $this->racesRepository->findAll(),
+                'choice_label' => 'name',
+                'expanded' => true,
+                'label'=> ' ',
+                'mapped'=>false
             ])
             ->add('idUsers', EntityType::class, [
                 'class' => User::class,
-'choice_label' => 'id',
+                'choice_label' => 'id',
             ])
             ->add('idLevel', EntityType::class, [
                 'class' => Levels::class,
-'choice_label' => 'id',
+                'choice_label' => 'id',
             ])
         ;
     }
