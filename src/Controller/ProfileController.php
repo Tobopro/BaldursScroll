840dc42f15471fd7ccee4dae51bd1b88b0f12597
Tom <tom.boyertb68@gmail.com>
2024-02-23 14:05:32 +0100

    profile-page-render

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\CharactersRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    #[Route('/profile/{idUser}', name: 'app_profile')]
+    public function show(int $idUser ,UserRepository $userRepository, CharactersRepository $charactersRepository): Response
     {
+
+        $user=$userRepository->find($idUser);
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+        $userCharacters = $charactersRepository->findBy(['idUsers' => $idUser]);
+        $numberOfCharacters = count($userCharacters);
+
+
+
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
+            'user' => $user,
+            'userCharacters' => $userCharacters,
+            'numberOfCharacters' => $numberOfCharacters
         ]);
     }
 }
