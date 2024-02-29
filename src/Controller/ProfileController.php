1c32421387223e6e7c7986432355293eb0a64aa5
zara-ahmadi <ahmadizahra897@gmail.com>
2024-02-29 10:13:45 +0100

    resolve the delete function for profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
-use App\Repository\CharactersRepository;
+use App\Entity\User;
+use App\Form\EditProfileType;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CharactersRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile/{idUser}', name: 'app_profile')]
-    public function show(int $idUser ,UserRepository $userRepository, CharactersRepository $charactersRepository): Response
+    public function show(int $idUser, UserRepository $userRepository, CharactersRepository $charactersRepository): Response
     {
 
-        $user=$userRepository->find($idUser);
+        $user = $userRepository->find($idUser);
         if (!$user) {
             throw $this->createNotFoundException('User not found');
         }
@@ -30,6 +35,73 @@ class ProfileController extends AbstractController
             'userCharacters' => $userCharacters,
             'numberOfCharacters' => $numberOfCharacters,
             'numberOfComments' => $numberOfComments
+        ]);
+    }
+
+    // delete profile
+    #[Route('profile/{id<\d*>}/delete', name: 'app_profile_delete')]
+    public function delete(EntityManagerInterface $entityManager, User $user): Response
+    {
+        $userPublic = $entityManager->getRepository(User::class)->find(1);
+
+        if (!$userPublic) {
+            throw $this->createNotFoundException("Utilisateur de remplacement non trouvé avec l\'id 1");
+        }
+
+        $characters = $user->getCharacters();
+        $commentaries = $user->getCommentaries();
+
+        foreach ($characters as $character) {
+            $character->setIdUsers($userPublic);
+        }
+
+        foreach ($commentaries as $commentary) {
+            $commentary->setAuthor($userPublic);
+        }
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_dashboard');
+    }
+
+
+    #[Route('/profile/{idUser}/edit', name: 'app_profile_edit')]
+    public function edit(EntityManagerInterface $entityManager, Request $request, int $idUser, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = $userRepository->find($idUser);
+        $form = $this->createForm(EditProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Check if current password is provided
+            $currentPassword = $form->get('currentPassword')->getData();
+            if ($currentPassword !== null) {
+                // Validate current password
+                if (!$passwordHasher->isPasswordValid($user, $currentPassword)) {
+                    $this->addFlash('error', 'Current password is incorrect.');
+                    return $this->redirectToRoute('app_profile_edit', ['idUser' => $user->getId()]);
+                }
+            }
+
+            // Update password if provided
+            $newPassword = $form->get('plainPassword')->getData();
+            if ($newPassword) {
+                $hashedPassword = $passwordHasher->hashPassword($user, $newPassword);
+                $user->setPassword($hashedPassword);
+            }
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'L\'utilisateur a bien été modifié');
+            return $this->redirectToRoute('app_profile', ['idUser' => $user->getId()]);
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'title' => "Modifier un utilisateur",
+            'editProfileForm' => $form,
         ]);
     }
 }
--- src/Form/EditProfileType.php
+++ src/Form/EditProfileType.php
@@ -5,48 +5,40 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
-
-class UserType extends AbstractType
+class EditProfileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-
             ->add('username', TextType::class, [
-                'label' => "Username"
+                'label' => "User Name",
+                'required' => false
             ])
             ->add('email', EmailType::class, [
-                'label' => "Email"
+                'label' => "Email",
+                'required' => false
             ])
-            ->add('password', RepeatedType::class, [
-                'type'          => PasswordType::class,
-                'first_options' => [
-                    'label' => 'Password'
-                ],
-                'second_options' => [
-                    'label' => 'Confirm your password'
-                ],
-                'constraints' => [
-                    new Length([
-                        'min' => 8,
-                        'minMessage' => 'the password should be 8 chars'
-                    ]),
-                ],
+            ->add('currentPassword', PasswordType::class, [
+                'label' => 'Current Password',
+                'mapped' => false,
+                'required' => false
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'  => ['label' => 'New Password'],
+                'second_options' => ['label' => 'Repeat New Password'],
+                'mapped' => false,
+                'required' => false
             ])
             ->add('save', SubmitType::class, [
-                'label' => 'Valider',
-                'attr' => [
-                    'class' => 'btn-primary w-100'
-                ]
+                'label' => 'Submit'
             ]);
     }
 
