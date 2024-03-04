65b54159dcd52ec7d23db2e802b627292cb90d76
Tom <tom.boyertb68@gmail.com>
2024-03-04 14:22:19 +0100

    array added with insults

--- migrations/Version20240304131518.php
+++ migrations/Version20240304131518.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240229124538 extends AbstractMigration
+final class Version20240304131518 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,19 +20,23 @@ final class Version20240229124538 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE characters ADD is_public TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE characters ADD is_flaged TINYINT(1) DEFAULT 0 NOT NULL');
+      
+      
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE classes_spells DROP INDEX UNIQ_A4A4FFA08F8F75EF, ADD INDEX FK_A4A4FFA08F8F75EF (id_sub_classes_id)');
-        $this->addSql('ALTER TABLE classes_spells DROP INDEX UNIQ_A4A4FFA017452598, ADD INDEX FK_A4A4FFA017452598 (id_spell_id)');
+        $this->addSql('ALTER TABLE characters DROP is_flaged');
+        $this->addSql('DROP INDEX UNIQ_A4A4FFA08F8F75EF ON classes_spells');
+        $this->addSql('DROP INDEX UNIQ_A4A4FFA017452598 ON classes_spells');
+        $this->addSql('CREATE INDEX UNIQ_A4A4FFA08F8F75EF ON classes_spells (id_sub_classes_id)');
+        $this->addSql('CREATE INDEX UNIQ_A4A4FFA017452598 ON classes_spells (id_spell_id)');
         $this->addSql('DROP INDEX UNIQ_C08D4AB1F6AA732 ON races_spells');
         $this->addSql('DROP INDEX UNIQ_C08D4AB1993450E ON races_spells');
         $this->addSql('CREATE INDEX UNIQ_C08D4AB1F6AA732 ON races_spells (id_level_id)');
         $this->addSql('CREATE INDEX UNIQ_C08D4AB1993450E ON races_spells (id_sub_race_id)');
-        $this->addSql('ALTER TABLE characters DROP is_public');
         $this->addSql('ALTER TABLE `user` CHANGE sign_in_date sign_in_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
     }
 }
