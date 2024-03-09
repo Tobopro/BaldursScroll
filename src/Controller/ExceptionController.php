46920e77187da1b673c386856ecfffe49ccfc3e0
zara-ahmadi <ahmadizahra897@gmail.com>
2024-03-09 11:46:39 +0100

    page 404 added

--- src/Controller/ExceptionController.php
+++ src/Controller/ExceptionController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class ExceptionController extends AbstractController
 {
-    public function showAccessDenied(AccessDeniedHttpException $exception): Response
+    public function showAccessDenied(HttpExceptionInterface $exception): Response
     {
-        return $this->render(
-            'access_denied.html.twig',
-            [],
-            new Response('', Response::HTTP_FORBIDDEN)
-        );
+        $statusCode = $exception->getStatusCode();
+
+        if ($statusCode === 404) {
+            $template = 'error/404.html.twig';
+        } elseif ($statusCode === 403) {
+            $template = 'error/403.html.twig';
+        }
+
+        return $this->render($template, [
+            'status_code' => $statusCode,
+            'status_text' => Response::$statusTexts[$statusCode] ?? 'Unknown Error',
+        ]);
     }
 }
